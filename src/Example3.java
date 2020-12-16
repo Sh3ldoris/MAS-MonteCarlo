ad1800c754dea9d5843ab7d4ad195ca19a290801
lanya <adam.lany@posam.sk>
2020-12-16 09:49:21 +0100

    Example 3 - add replications

--- src/Example3.java
+++ src/Example3.java
@@ -11,29 +11,36 @@ public class Example3 {
         UniformContinuousRNG xGen = new UniformContinuousRNG();
         UniformContinuousRNG yGen = new UniformContinuousRNG();
 
-        boolean isPi = false;
-        int inCircle = 0;
         int total = 0;
-
         double estimatePi = 0;
 
-        while (true) {
-            double x = xGen.sample();
-            double y = yGen.sample();
+        final int replications = 100;
 
-            if (isInCircle(x, y)) {
-                inCircle++;
+        //Netusim ci su tu vhodne replikacie
+        for (int i = 0; i < replications; i++) {
+            int totalPerTry = 0;
+            int inCircle = 0;
+
+            while (true) {
+                double x = xGen.sample();
+                double y = yGen.sample();
+
+                if (isInCircle(x, y)) {
+                    inCircle++;
+                }
+
+                total++;
+                totalPerTry++;
+                double area = (double) inCircle / totalPerTry;
+
+                estimatePi = (area / Math.pow(r, 2));
+                if (Math.abs((estimatePi - Math.PI)) < 0.000001)
+                    break;
             }
-
-            total++;
-            double area = (double) inCircle / total;
-
-            estimatePi = (area / Math.pow(r, 2));
-            if (Math.abs((estimatePi - Math.PI)) < 0.000001)
-                break;
+            System.out.println(i + ". ---> " + totalPerTry);
         }
 
-        System.out.println("Pi je: " + estimatePi + " ------ a bolo potrebnych umiestnit " + total + " bodov!");
+        System.out.println("Bolo potrebnych umiestnit " + (double) total / replications + " bodov!");
     }
 
     private static boolean isInCircle(double x, double y) {
