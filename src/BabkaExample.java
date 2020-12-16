1d4fa0c682a45f0d14dfe240c4e6cc564483ff14
lanya <adam.lany@posam.sk>
2020-12-16 15:34:53 +0100

    Uprava

--- src/BabkaExample.java
+++ src/BabkaExample.java
@@ -10,23 +10,30 @@ public class BabkaExample {
 
         final int replications = 1000000;
         double totalProfit = 0;
-        double totalNotSold = 0;
+        int totalNotSold = 0;
+
         for (int i = 0; i < replications; i++) {
             int gingerBreadC = 100;
             double workTime = 480;
             double price = priceGen.sample();
 
             while (workTime > 0) {
-                if (gingerBreadC == 0)
+                double timeBetween = 0;
+                if (gingerBreadC == 0 )
                     break;
 
                 if (workTime <= 60 && gingerBreadC >= 10) {
-                    workTime -= shortTimeGen.sample();
-                    totalProfit += (price * 0.25);
+                    timeBetween = shortTimeGen.sample();
+                    price = (price * 0.25);
                 } else {
-                    workTime -= timeGen.sample();
-                    totalProfit += price;
+                    timeBetween = timeGen.sample();
                 }
+
+                if ((workTime - timeBetween) < 0)
+                    break;
+
+                workTime -= timeBetween;
+                totalProfit += price;
                 gingerBreadC--;
             }
 
@@ -35,7 +42,7 @@ public class BabkaExample {
 
         System.out.println("************************************************");
         System.out.println("Babke priemerne ostane --> " + (double) totalNotSold / replications);
-        System.out.println("Babka priemerne zarobi --> " + (double) totalProfit / replications);
+        System.out.println("Babka priemerne zarobi --> " + totalProfit / replications);
 
     }
 }
